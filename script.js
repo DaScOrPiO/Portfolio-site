07f5416a15a2d36c60279fa7f69af090c6645523
DaScOrPiO <dunni82@yahoo.com>
2022-08-11 10:18:49 -0700

    3rd commit, Minor correction to responsive nav

--- script.js
+++ script.js
@@ -3,31 +3,33 @@ const navMenu = document.querySelector('.nav-menu');
 const firstInputSet = document.querySelector('.text-input');
 const secondInputSet = document.querySelector('.input')
 const formbtn = document.querySelector('input[type=Submit]');
-const linkEl = document.querySelector('.nav-link');
+const linkEl = navMenu.querySelectorAll('.nav-link');
 const header = document.querySelector('.header');
 const darkModebtn = document.querySelector('.dark');
 
 function mobileMenu() {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-    linkEl.addEventListener('click', (() => {
-        navMenu.classList.remove('active');
-        hamburger.classList.remove('active');
-    }))
+    linkEl.forEach((e) => {
+        e.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+            hamburger.classList.remove('active');
+        })
+    })
 }
 hamburger.addEventListener('click', mobileMenu);
 
-function validateForm(){
+function validateForm() {
     let content1 = firstInputSet.value;
     let content2 = firstInputSet.value;
     let text;
-    if(content1 === '' || content2 === ''){
+    if (content1 === '' || content2 === '') {
         alert('No Input detected');
     }
 }
 formbtn.addEventListener('click', validateForm);
 
-function backgroundSwitch(){
+function backgroundSwitch() {
     darkModebtn.addEventListener('click', (() => {
         document.body.classList.toggle('dark-mode');
     }))
